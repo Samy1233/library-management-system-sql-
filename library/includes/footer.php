81ae4975eafe87f8ca6251981036c27729ad9dba
Samy1233 <sjsamyak4@gmail.com>
2024-12-16 18:00:10 +0530

    Initial commit

---
+++ library/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; Library Management System |<a href="https://github.com/Samy1233" target="_blank"  > Designed by : Samyak&Nimbraj 2024 <br> contact :9075076542S</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
